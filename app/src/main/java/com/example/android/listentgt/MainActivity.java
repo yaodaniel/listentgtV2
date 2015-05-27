50bdb454786b014dce15d3286562b4201eb65984
guanbeiqi@gmail.com <guanbeiqi@jiuzhilan.net>
2015-05-26 17:41:09 -0700

    Connectivity integrated

--- app/src/main/java/com/example/android/listentgt/MainActivity.java
+++ app/src/main/java/com/example/android/listentgt/MainActivity.java
@@ -30,7 +30,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class MainActivity extends FragmentActivity implements ActionBar.TabListener{
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.widget.Toast;
+
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener, WifiP2pManager.ChannelListener{
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide fragments for each of the
@@ -45,8 +53,26 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
      * time.
      */
     ViewPager mViewPager;
-    static FragmentPlayList playlistFrag;
+//    static FragmentPlayList playlistFrag;
     String[] pages = new String[]{"Play", "Connect", "Settings"};
+
+    //WiFiP2pManager variables
+    public static final String TAG = "ListenTGT";
+    private WifiP2pManager manager;
+    private boolean isWifiP2pEnabled = true;
+    private boolean retryChannel = false;
+
+    private final IntentFilter intentFilter = new IntentFilter();
+    private WifiP2pManager.Channel channel;
+    private BroadcastReceiver receiver = null;
+
+    //keep track of fragments
+    private static Fragment fragment1 = new FragmentMusicPlayer();
+    private static Fragment fragment2 = new DeviceListFragment();
+    //settings fragment to be set
+    private static Fragment fragment3;
+
+
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -90,7 +116,25 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
                             .setText(pages[i])
                             .setTabListener(this));
         }
+
+        //wifiP2pManagerActivity
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
     }
+
+    public static Fragment getFragment1() {
+        return fragment1;
+    }
+
+    public static Fragment getFragment2() {
+        return fragment2;
+    }
+
 
     @Override
     public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
@@ -126,10 +170,11 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         public Fragment getItem(int i) {
             switch (i) {
                 case 0:
-                    // The first section of the app is the most interesting -- it offers
-                    // a launchpad into the other demonstrations in this example application.
-                    playlistFrag = new FragmentPlayList();
-                    return playlistFrag;
+
+                    return fragment1;
+
+                case 1:
+                    return fragment2;
                 default:
                     // The other sections of the app are dummy placeholders.
                     Fragment fragment = new DummySectionFragment();
@@ -151,6 +196,38 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         }
     }
 
+    //button click function
+    public boolean initiateDiscovery(View view)
+    {
+        if (!isWifiP2pEnabled) {
+            Toast.makeText(MainActivity.this, R.string.p2p_off_warning,
+                    Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        final DeviceListFragment fragment = (DeviceListFragment) getFragment2();
+        fragment.onInitiateDiscovery(this);
+
+        //discovery
+        manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+
+            @Override
+            public void onSuccess() {
+                Toast.makeText(MainActivity.this, "Discovery Initiated",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+                Toast.makeText(MainActivity.this, "Discovery Failed : " + reasonCode,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        return true;
+    }
+
+
+
+    //the dummy section is not useful
     /**
      * A dummy fragment representing a section of the app, but that simply displays dummy text.
      */
@@ -167,5 +244,59 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
                     getString(R.string.dummy_section_text, args.getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
+    }
+
+    //WifiP2Pmanager part
+
+    @Override
+    public void onChannelDisconnected() {
+        // we will try once more
+        if (manager != null && !retryChannel) {
+            Toast.makeText(this, "Channel lost. Trying again", Toast.LENGTH_LONG).show();
+//            resetData();
+            retryChannel = true;
+            manager.initialize(this, getMainLooper(), this);
+        } else {
+            Toast.makeText(this,
+                    "Severe! Channel is probably lost premanently. Try Disable/Re-Enable P2P.",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void setIsWifiP2pEnabled(boolean isWifiP2pEnabled) {
+        this.isWifiP2pEnabled = isWifiP2pEnabled;
+    }
+
+    /**
+     * Remove all peers and clear all fields. This is called on
+     * BroadcastReceiver receiving a state change event.
+     */
+    public void resetData() {
+//        DeviceListFragment fragmentList = (DeviceListFragment) getFragmentManager()
+//                .findFragmentById(R.id.frag_list);
+//        DeviceDetailFragment fragmentDetails = (DeviceDetailFragment) getFragmentManager()
+//                .findFragmentById(R.id.frag_detail);
+//        if (fragmentList != null) {
+//            fragmentList.clearPeers();
+//        }
+//        if (fragmentDetails != null) {
+//            fragmentDetails.resetViews();
+//        }
+
+        //do nothing yet
+    }
+
+    /** register the BroadcastReceiver with the intent values to be matched */
+    @Override
+    public void onResume() {
+        super.onResume();
+        receiver = new WiFiDirectBroadcastReceiver(manager, channel, this);
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
     }
 }
